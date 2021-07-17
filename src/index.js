859564173e2952b4d55a1ac0ff971d1533f9a6ee
mdquayle <misternopants@gmail.com>
2021-07-17 09:48:27 -0400

    changed API source

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
 //DOMContentLoaded will make sure the HTML finishes downloading first before any of my JavaScript is run
 
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    const search = document.querySelector('form')
-    search.addEventListener('submit', (event) => {
-        event.preventDefault();
-        search.reset()
-        document.getElementById('submit').addEventListener('click', getDrinks)
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM fully loaded and parsed')})
+
+const search = document.querySelector('form')
+search.addEventListener('submit', (event) => {
+    event.preventDefault();
+    search.reset()
+    document.getElementById('submit').addEventListener('click', getBreweries)
 //preventDefault() is to prevent the page from automatically reloading when the submit button is hit
 //search.reset() emptys the text field after submitting
-        })
+        
 });
 
 
-function getDrinks(){
+function getBreweries(){
     let searchValue = document.getElementById("searchValue").value
-    let drinkList = document.getElementById("Cocktail")
+    let drinkList = document.getElementById("Breweries")
     // console.log(searchValue)
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${searchValue}`)
+    fetch("https://api.openbrewerydb.org/breweries")
     .then(res => res.json())
     .then(results => 
         {for(i = 0; i < results.drinks.length; i++){
@@ -26,15 +28,5 @@ function getDrinks(){
                     <a href=# data-id>${results.drinks[i].strDrink}</a>
                 </li> `
         
-    }})
-    dataset.id.addEventListener('click', getRecipe)
-}
-function getRecipe(){
-    fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${searchValue}")
-    .then(res => res.json())
-    .then(results => {
-        results.drinks[`${dataset-id}`].str
-    })
-}
-
+    }})}
 //${dataset-id} to call the data-id in the a tag
